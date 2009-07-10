e7109cb8fdca1c94c730dc6e9ac7cddde6804f1e
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-07-11 01:00:31 +1000

    added feed test

--- test/functional/basic/feed_test.py
+++ test/functional/basic/feed_test.py
@@ -1,3 +1,4 @@
+import cgi
 from mocktest import *
 
 from models.page import Page
@@ -5,7 +6,23 @@ from models.user import UserID
 from pagefeed.test import fixtures
 
 class FeedTest(TestCase):
+	def get_feed(self, email, secret, **kwargs):
+		email = cgi.escape(str(email))
+		secret = cgi.escape(str(secret))
+		return fixtures.app().get('/feed/%s-%s/' % (secret, email), **kwargs)
+		
 	def test_should_display_feeds_for_a_user(self):
-		pass
-		# mock_on(page).find_all.is_expected.with_()#TODO
+		email = fixtures.a_user.email()
+		userid = UserID.get(email)
+		page = fixtures.stub_page()
+		page.title = 'the title!'
+		mock_on(Page).find_all.is_expected.with_(fixtures.a_user)
+		response = self.get_feed(email, userid.handle)
+		response.mustcontain('the title!')
+
+	def test_should_not_display_feeds_for_an_illegitimate_user(self):
+		email = fixtures.a_user.email()
+		userid = UserID.get(email)
+		mock_on(Page).find_all.is_not_expected
+		self.get_feed(email, '123', status=403)
 
