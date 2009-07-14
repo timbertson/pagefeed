b42e59420555bcb633d751a90dd067bf3e1c08be
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-07-14 14:18:32 +1000

    got ajax somewhat-working

--- public/ajax-forms.js
+++ public/ajax-forms.js
@@ -1,42 +1,66 @@
 var ajax_reqs = 0
-function pushAjax() { ajax_reqs += 1; if(ajax_reqs == 1) throb(); }
-function popAjax()  { ajax_reqs -= 1; if(ajax_reqs == 0) stopThrob(); }
+function pushAjax(elm) { ajax_reqs += 1; if(ajax_reqs == 1) throb(); }
+function popAjax(elm)  { ajax_reqs -= 1; if(ajax_reqs == 0) stopThrob(); }
 
 function throb() {   $("throb").show(100); }
 function stopThrob() { $("throb").hide(100); }
 
+function fadeRepace(elem, replacement) {
+	var replaceIt = function(){
+		replacement.css('opacity',0.5)
+		elem.replaceWith(replacement);
+		$(replacement).animate({'opacity':1, 'speed':500});
+	}
+	elem.animate({'opacity':0.5, 'speed':500}, "easeOutQuad", replaceIt);
+}
+
 function ajaxify(base) {
 	if (!base) { base = document.body; }
-	$("form .ajax", base).submit(function() {
-		alert("submitting!");
+	$("form.ajax", base).submit(function() {
 		frm = $(this);
-		if($("input[name=ajax]")) {
-			return;
+		if($("input[name=ajax]", frm).length > 0) {
+			alert("already submitted!");
+			return false;
 		}
-		frm.prepend("<input name=\"ajax\" type=\"hidden\" value=\"true\" />");
-		pushAjax();
+		alert("submitting!");
+		ajaxFlag = $("<input name=\"ajax\" type=\"hidden\" value=\"true\" />", document)
+		frm.prepend(ajaxFlag);
+		pushAjax(frm);
+		console.log("submitting:" + frm.attr("action"));
+		console.log("via:" + frm.attr("method"));
+		console.log("data: " + frm.serialize());
 		$.ajax({
-			url: frm.action,
-			type: frm.method,
+			url: frm.attr("action"),
+			type: frm.attr("method"),
 			data: frm.serialize(),
 			dataType: "html",
-			complete: popAjax,
+			complete: function() { ajaxFlag.remove(); popAjax(frm); },
 			error: function(xhr, textStatus, err) {
 				alert("fail!");
 			},
 			success: function(data, status) {
-				target_sel = $("input[name=dest]", frm).value;
+				data = "<h2>TADA!</h2>"
+				console.log("Success!");
+				console.log(status);
+				console.log(data);
+				target_sel = $("input[name=dest]", frm).val();
 				if(target_sel[0] == '#'){
 					target = $(target_sel);
 				} else {
 					target = frm.closest(target_sel);
 				}
-			
+				console.log("target: " + target);
 				html = $(data, document);
+				console.log("html: " + html);
 				ajaxify(html);
-				meth = $("input[name=dest_type]", frm).value;
+				meth = $("input[name=meth]", frm).val();
 				if(meth == "replace") {
-					target.replace(html);
+					fadeRepace(target, html);
+				} else if(meth == "remove") {
+					if(data.length > 0) {
+						console.error("replacement HTML is nonzero: " + data);
+					}
+					target.hide(1000, function(){target.remove()});
 				} else {
 					html.hide();
 					if(meth == "before"){
@@ -48,7 +72,9 @@ function ajaxify(base) {
 				}
 			},
 		});
-		return true; // stop propagation
+		return false; // stop propagation
 	});
 	return "done"
 }
+
+$(function(){ajaxify()});
