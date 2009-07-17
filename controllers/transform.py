db0e7fbf0e8e2e5e8593de2f2e0d7c68dd3d252b
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-07-17 21:06:17 +1000

    added working transform page

--- controllers/transform.py
+++ controllers/transform.py
@@ -4,7 +4,24 @@ from base import *
 from models import Transform
 
 class TransformHandler(BaseHandler):
+	root = "/transform/"
 	def get(self):
 		transforms = Transform.find_all(self.user())
-		render_page('transforms', {'transforms':transforms})
+		self.response.out.write(render_page('transforms', {'transforms':transforms}))
+
+	def transform_params(self):
+		excluded_properties = ('version', 'index')
+		include = lambda key: not (key in excluded_properties or key.startswith('_'))
+		return filter(include, Transform.properties())
+
+	def post(self):
+		required_params = self.transform_params()
+		transform_params = dict(owner=self.user())
+		for param in required_params:
+			if param in transform_params: continue
+			value = self.request.get(param)
+			if not value:
+				raise ValueError(param)
+			transform_params[param] = value
+		self.redirect(self.root)
 
