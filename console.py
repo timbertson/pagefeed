88d5079f16da8b5a00473b0a258a8fe48d2b9ee7
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-07-19 12:40:36 +1000

    added a functional GAE console

--- console.py
+++ console.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python2.5
 import sys, os
-appengine = '/usr/local/google_appengine'
-libs = appengine + '/lib/'
-sys.path.append(appengine)
+import tempfile
 
-for lib in os.listdir(libs):
-	sys.path.append(libs + lib)
+def add_load_path(p):
+	if not p in sys.path:
+		sys.path.append(p)
+
+appengine = '/usr/local/google_appengine'
+def add_gae_paths():
+	libs = appengine + '/lib/'
+	paths = [appengine, libs] + [libs + d for d in os.listdir(libs)]
+	map(add_load_path, paths)
+
+def init_gae():
+	from google.appengine.tools import dev_appserver
+	from google.appengine.tools.dev_appserver_main import \
+		DEFAULT_ARGS, ARG_CLEAR_DATASTORE, ARG_LOG_LEVEL, \
+		ARG_DATASTORE_PATH, ARG_HISTORY_PATH
+	# prefill these into sys.modules
+	import webob
+	import yaml
+	import django
+
+	# and start GAE!
+	gae_opts = DEFAULT_ARGS.copy()
+	gae_opts[ARG_CLEAR_DATASTORE] = False
+	gae_opts[ARG_DATASTORE_PATH] = os.path.join(tempfile.gettempdir(),
+		'nosegae.datastore')
+	gae_opts[ARG_HISTORY_PATH] = os.path.join(tempfile.gettempdir(),
+		'gae.datastore.history')
+	config, _junk = dev_appserver.LoadAppConfig(os.curdir, {})
+	dev_appserver.SetupStubs(config.application, **gae_opts)
+
+def gae():
+	add_gae_paths()
+	init_gae()
+
