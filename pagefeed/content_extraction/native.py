abc2228c05b9986af3d544c32ec1471c707aac7a
gfxmonk <tim@gfxmonk.net>
2011-03-07 18:44:59 +1100

    fix queue issue; make all parse errors throw non-retryable exceptions

--- pagefeed/content_extraction/native.py
+++ pagefeed/content_extraction/native.py
@@ -7,12 +7,12 @@ def extract(page):
 	url = page.content_url
 	content = Content(url=url, source='native')
 	logging.info("fetching %r with native extractor" % (url,))
+	body = page.raw_content
 	try:
-		body = page.raw_content
 		soup = page_parser.parse(body, base_href=page.base_href, notify=logging.info)
 		content.body = page_parser.get_body(soup)
 		content.title = page_parser.get_title(soup)
-	except (page_parser.Unparseable, AttributeError), e:
+	except StandardError, e:
 		raise deferred.PermanentTaskFailure("%s: %s" % (type(e), e))
 	content.put()
 	logging.info("fetched %r with native extractor, got content size %s" % (url,content.size))
